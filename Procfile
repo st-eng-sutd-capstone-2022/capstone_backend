bc6af7c86f4ce00158c2532f107bf528b83885c6
Dody Senputra 李伟胜 <dodysenz@gmail.com>
2022-06-07 16:46:23 +0800

    add keep alive script in heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: npm run start:prod
 worker: npm run start:prod:mqtt
+worker: node ./mockMqtt/start-keep-alive.js
--- mockMqtt/start-keep-alive.js
+++ mockMqtt/start-keep-alive.js
@@ -5,13 +5,6 @@ const client = mqtt.connect('mqtt://broker.hivemq.com:1883');
 
 let TOPIC = `keep-alive`;
 
-let i = 0;
-
-client.subscribe(TOPIC);
-client.on('message', function (topic, message) {
-  console.log(`${i++} topic: ${topic}`, JSON.parse(message.toString()));
-});
-
 const publish = (t) => {
   const payload = {
     timestamp: Date.now(),
