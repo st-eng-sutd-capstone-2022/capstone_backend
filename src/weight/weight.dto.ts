fc468a837631022c36fad3a0bfa15034a6409936
Dody Senputra 李伟胜 <dodysenz@gmail.com>
2022-07-23 14:34:31 +0800

    clean up weight schema

--- src/weight/weight.dto.ts
+++ src/weight/weight.dto.ts
@@ -2,15 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsString, IsNumber } from 'class-validator';
 
 export class WeightDTO {
-  //   @ApiProperty()
-  //   @IsString()
-  //   boatId: string;
-
-  //   @ApiProperty()
-  //   @IsDate()
-  //   newTimestamp: Date;
-
   @ApiProperty()
   @IsNumber()
-  newWeight: Number;
+  newWeight: number;
 }
